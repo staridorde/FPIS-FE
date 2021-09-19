d151bb650c6c1263272f78c44c8f1c51d29755a8
miapecelj <miapecelj16@gmail.com>
2021-09-19 16:53:34 +0200

    empty2

--- src/components/klijent/UnosKlijenta.vue
+++ src/components/klijent/UnosKlijenta.vue
@@ -31,17 +31,26 @@
         <el-option
             v-for="var_ulica in ulice"
             :key="var_ulica.id"
-            :label="var_ulica.naziv"
-            :value="var_ulica.naziv"
+            :label="var_ulica.street"
+            :value="var_ulica"
         >
         </el-option>
     </el-select>
     <el-select v-model="sifraDelatnosti" placeholder="Odaberi sifru delatnosti">
         <el-option
-            v-for="var_delatnos in delatnosti"
-            :key="var_delatnos.code"
-            :label="var_delatnos.name"
-            :value="var_delatnos"
+            v-for="var_delatnost in delatnosti"
+            :key="var_delatnost.code"
+            :label="var_delatnost.name"
+            :value="var_delatnost"
+        >
+        </el-option>
+    </el-select>
+    <el-select v-model="potencijalniKlijent" placeholder="Odaberi potencijalnog klijenta">
+        <el-option
+            v-for="var_klijent in potencijalniKlijenti"
+            :key="var_klijent.id"
+            :label="var_klijent.name"
+            :value="var_klijent"
         >
         </el-option>
     </el-select>
@@ -50,12 +59,6 @@
     </el-form-item> -->
     <el-form-item label="Broj">
         <el-input v-model="broj"></el-input>
-    </el-form-item>
-    <el-form-item label="Sifra delatnosti">
-        <el-input v-model="sifraDelatnosti"></el-input>
-    </el-form-item>
-    <el-form-item label="Potencijalni klijent">
-        <el-input v-model="potencijalniKlijent"></el-input>
     </el-form-item>
     <el-form-item>
         <el-button type="primary" @click="submitForm()">Unesi</el-button>
@@ -78,12 +81,13 @@ export default {
           broj: '',
           sifraDelatnosti: '',
           potencijalniKlijent: '',
-          mesto: '',
+          mesto: undefined,
         })
 
         const mesta = ref([])
         const ulice = ref([])
         const delatnosti = ref([])
+        const potencijalniKlijenti = ref([])
 
         const postClientObjectTemplate = {
             PIB: '',
@@ -124,34 +128,40 @@ export default {
 
         
         fetch('http://localhost:8080/vip/api/potentialClient')
-            .then((response) => response.text())
-            .then((data) => console.log(data))
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data) 
+                potencijalniKlijenti.value = data._embedded.potentialClient
+            })
             .catch((error) => console.log(error))
     
         const handleCitySelected = () => {
-            fetch(`http://localhost:8080/vip/client/${form.mesto.broj}`, {
-                method: 'GET',
-                mode: 'no-cors'
-            })
+            fetch(`http://localhost:8080/vip/client/address/${form.mesto.postalCode}`)
                 .then((response) => response.json())
-                .then((data) => console.log(data))
+                .then((data) => {
+                    ulice.value = data
+                })
                 .catch((error) => console.log(error))
         }
 
         const submitForm = () => {
+            console.log(JSON.parse(JSON.stringify(form)))
             postClientObjectTemplate.PIB = form.pib
             postClientObjectTemplate.name = form.nazivKlijenta
             postClientObjectTemplate.webPage = form.webStrana
             postClientObjectTemplate.phone = form.telefon
-            postClientObjectTemplate.yearOfEstablishment = form.godinaOsnivanja
+            postClientObjectTemplate.yearOfEstablishment = parseInt(form.godinaOsnivanja)
             postClientObjectTemplate.address.id = form.ulica.id
-            postClientObjectTemplate.address.number = form.broj
-            postClientObjectTemplate.city.postalCode = form.mesto.postanskiBroj
+            postClientObjectTemplate.address.number = parseInt(form.broj)
+            postClientObjectTemplate.city.postalCode = form.mesto.postalCode
             postClientObjectTemplate.potentialClient.id = form.potencijalniKlijent.id
-            postClientObjectTemplate.activity.code = form.sifraDelatnosti
+            postClientObjectTemplate.activity.code = form.sifraDelatnosti.code
             console.log(postClientObjectTemplate)
             fetch('http://localhost:8080/vip/api/client', {
                 method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
                 body: JSON.stringify(postClientObjectTemplate)
             })
                 .then((response) => console.log(response))
@@ -165,6 +175,7 @@ export default {
             mesta,
             ulice,
             delatnosti,
+            potencijalniKlijenti,
         }
     }
 }
